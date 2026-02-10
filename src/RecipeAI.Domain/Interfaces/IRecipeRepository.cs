782fc1e09753eac965efd374bcc327fb4df4d3bd
thekcr85 <thekcr85@gmail.com>
2026-02-10 19:25:29 +0100

    Add IRecipeRepository interface contract

--- src/RecipeAI.Domain/Interfaces/IRecipeRepository.cs
+++ src/RecipeAI.Domain/Interfaces/IRecipeRepository.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using RecipeAI.Domain.Entities;
 
-namespace RecipeAI.Domain.Interfaces
+namespace RecipeAI.Domain.Interfaces;
+
+/// <summary>
+/// Repository interface for recipe operations
+/// </summary>
+public interface IRecipeRepository
 {
-	internal class IRecipeRepository
-	{
-	}
+	/// <summary>
+	/// Gets a recipe by its identifier including ingredients
+	/// </summary>
+	/// <param name="id">The recipe identifier</param>
+	/// <param name="cancellationToken">Cancellation token</param>
+	/// <returns>The recipe if found, otherwise null</returns>
+	Task<Recipe?> GetByIdAsync(int id, CancellationToken cancellationToken = default);
+
+	/// <summary>
+	/// Gets all recipes for a specific meal plan
+	/// </summary>
+	/// <param name="mealPlanId">The meal plan identifier</param>
+	/// <param name="cancellationToken">Cancellation token</param>
+	/// <returns>Collection of recipes for the meal plan</returns>
+	Task<IEnumerable<Recipe>> GetByMealPlanIdAsync(int mealPlanId, CancellationToken cancellationToken = default);
 }
