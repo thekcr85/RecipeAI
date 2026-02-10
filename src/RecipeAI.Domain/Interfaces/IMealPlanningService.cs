9eb13af44af31ff716f186e514cf747a8d4ecdb7
thekcr85 <thekcr85@gmail.com>
2026-02-10 19:27:01 +0100

    Add IMealPlanningService interface contract

--- src/RecipeAI.Domain/Interfaces/IMealPlanningService.cs
+++ src/RecipeAI.Domain/Interfaces/IMealPlanningService.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using RecipeAI.Domain.Entities;
+using RecipeAI.Domain.Enums;
 
-namespace RecipeAI.Domain.Interfaces
+namespace RecipeAI.Domain.Interfaces;
+
+/// <summary>
+/// Service interface for AI-powered meal planning operations
+/// </summary>
+public interface IMealPlanningService
 {
-	internal class IMealPlanningService
-	{
-	}
+	/// <summary>
+	/// Generates a meal plan using AI agents with iterative refinement
+	/// </summary>
+	/// <param name="dietType">The desired diet type</param>
+	/// <param name="numberOfDays">Number of days to plan for</param>
+	/// <param name="targetCalories">Daily calorie target</param>
+	/// <param name="budgetLimit">Maximum budget for the plan</param>
+	/// <param name="cancellationToken">Cancellation token</param>
+	/// <returns>The generated meal plan with planning session details</returns>
+	Task<MealPlan> GenerateMealPlanAsync(
+		DietType dietType,
+		int numberOfDays,
+		int targetCalories,
+		decimal budgetLimit,
+		CancellationToken cancellationToken = default);
 }
