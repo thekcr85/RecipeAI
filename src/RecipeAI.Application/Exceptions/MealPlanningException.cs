d0c9684969d15dfa8f0dc7e2495a2656f926ed17
thekcr85 <thekcr85@gmail.com>
2026-02-11 15:23:31 +0100

    Add MealPlanningException for meal planning errors

--- src/RecipeAI.Application/Exceptions/MealPlanningException.cs
+++ src/RecipeAI.Application/Exceptions/MealPlanningException.cs
@@ -1,12 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace RecipeAI.Application.Exceptions;
 
-namespace RecipeAI.Application.Exceptions
+/// <summary>
+/// Exception thrown when meal planning operations fail
+/// </summary>
+public class MealPlanningException : Exception
 {
-	internal class MealPlanningException
+	/// <summary>
+	/// Initializes a new instance of the <see cref="MealPlanningException"/> class
+	/// </summary>
+	public MealPlanningException()
+	{
+	}
+
+	/// <summary>
+	/// Initializes a new instance of the <see cref="MealPlanningException"/> class with a message
+	/// </summary>
+	/// <param name="message">The error message</param>
+	public MealPlanningException(string message) : base(message)
+	{
+	}
+
+	/// <summary>
+	/// Initializes a new instance of the <see cref="MealPlanningException"/> class with a message and inner exception
+	/// </summary>
+	/// <param name="message">The error message</param>
+	/// <param name="innerException">The inner exception</param>
+	public MealPlanningException(string message, Exception innerException) : base(message, innerException)
 	{
 	}
 }
