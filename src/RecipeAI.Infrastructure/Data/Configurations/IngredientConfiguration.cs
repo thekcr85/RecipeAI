92157c39c02e14390f8c004b710a183cf92589b1
thekcr85 <thekcr85@gmail.com>
2026-02-11 13:04:40 +0100

    Add IngredientConfiguration for EF core mapping

--- src/RecipeAI.Infrastructure/Data/Configurations/IngredientConfiguration.cs
+++ src/RecipeAI.Infrastructure/Data/Configurations/IngredientConfiguration.cs
@@ -1,12 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RecipeAI.Domain.Entities;
 
-namespace RecipeAI.Infrastructure.Data.Configurations
+namespace RecipeAI.Infrastructure.Data.Configurations;
+
+/// <summary>
+/// Entity configuration for Ingredient
+/// </summary>
+public class IngredientConfiguration : IEntityTypeConfiguration<Ingredient>
 {
-	internal class IngredientConfiguration
+	/// <summary>
+	/// Configures the Ingredient entity
+	/// </summary>
+	/// <param name="builder">Entity type builder</param>
+	public void Configure(EntityTypeBuilder<Ingredient> builder)
 	{
+		builder.ToTable("Ingredients");
+
+		builder.HasKey(i => i.Id);
+
+		builder.Property(i => i.Name)
+			.IsRequired()
+			.HasMaxLength(100);
+
+		builder.Property(i => i.Unit)
+			.IsRequired()
+			.HasMaxLength(20);
+
+		builder.Property(i => i.Quantity)
+			.HasPrecision(8, 2);
+
+		builder.Property(i => i.Cost)
+			.HasPrecision(8, 2);
+
+		builder.HasIndex(i => i.Name);
 	}
 }
