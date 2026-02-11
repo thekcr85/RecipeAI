be228ffe0c45b95cc4f1c4467e507854b80a71c9
thekcr85 <thekcr85@gmail.com>
2026-02-11 12:59:24 +0100

    Add RecipeAIDbContext with DbSets and model configuration

--- src/RecipeAI.Infrastructure/Data/RecipeAIDbContext.cs
+++ src/RecipeAI.Infrastructure/Data/RecipeAIDbContext.cs
@@ -1,12 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using RecipeAI.Domain.Entities;
 
-namespace RecipeAI.Infrastructure.Data
+namespace RecipeAI.Infrastructure.Data;
+
+/// <summary>
+/// Database context for RecipeAI application
+/// </summary>
+/// <param name="options">Database context options</param>
+public class RecipeAIDbContext(DbContextOptions<RecipeAIDbContext> options) : DbContext(options)
 {
-	internal class RecipeAIDbContext
+	/// <summary>
+	/// Gets or sets the meal plans collection
+	/// </summary>
+	public DbSet<MealPlan> MealPlans => Set<MealPlan>();
+
+	/// <summary>
+	/// Gets or sets the recipes collection
+	/// </summary>
+	public DbSet<Recipe> Recipes => Set<Recipe>();
+
+	/// <summary>
+	/// Gets or sets the ingredients collection
+	/// </summary>
+	public DbSet<Ingredient> Ingredients => Set<Ingredient>();
+
+	/// <summary>
+	/// Gets or sets the planning sessions collection
+	/// </summary>
+	public DbSet<PlanningSession> PlanningSessions => Set<PlanningSession>();
+
+	/// <summary>
+	/// Configures the database model
+	/// </summary>
+	/// <param name="modelBuilder">Model builder instance</param>
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
 	{
+		base.OnModelCreating(modelBuilder);
+
+		modelBuilder.ApplyConfigurationsFromAssembly(typeof(RecipeAIDbContext).Assembly);
 	}
 }
