5eb869578084197b648c8e17dfc1ff6a10833003
thekcr85 <thekcr85@gmail.com>
2026-02-11 13:02:59 +0100

    Add MealPlanConfiguration for EF Core mapping

--- src/RecipeAI.Infrastructure/Data/Configurations/MealPlanConfiguration.cs
+++ src/RecipeAI.Infrastructure/Data/Configurations/MealPlanConfiguration.cs
@@ -1,12 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RecipeAI.Domain.Entities;
 
-namespace RecipeAI.Infrastructure.Data.Configurations
+namespace RecipeAI.Infrastructure.Data.Configurations;
+
+/// <summary>
+/// Entity configuration for MealPlan
+/// </summary>
+public class MealPlanConfiguration : IEntityTypeConfiguration<MealPlan>
 {
-	internal class MealPlanConfiguration
+	/// <summary>
+	/// Configures the MealPlan entity
+	/// </summary>
+	/// <param name="builder">Entity type builder</param>
+	public void Configure(EntityTypeBuilder<MealPlan> builder)
 	{
+		builder.ToTable("MealPlans");
+
+		builder.HasKey(mp => mp.Id);
+
+		builder.Property(mp => mp.Name)
+			.IsRequired()
+			.HasMaxLength(200);
+
+		builder.Property(mp => mp.DietType)
+			.IsRequired()
+			.HasConversion<string>();
+
+		builder.Property(mp => mp.TotalCost)
+			.HasPrecision(10, 2);
+
+		builder.Property(mp => mp.CreatedAt)
+			.IsRequired();
+
+		builder.HasMany(mp => mp.Recipes)
+			.WithOne(r => r.MealPlan)
+			.HasForeignKey(r => r.MealPlanId)
+			.OnDelete(DeleteBehavior.Cascade);
+
+		builder.HasOne(mp => mp.PlanningSession)
+			.WithOne(ps => ps.FinalMealPlan)
+			.HasForeignKey<MealPlan>(mp => mp.PlanningSessionId)
+			.OnDelete(DeleteBehavior.SetNull);
 	}
 }
