bc09ddc7641ca2127c024cf6e23f1f2be0540773
thekcr85 <thekcr85@gmail.com>
2026-02-28 09:59:16 +0100

    Add IPlanningSessionRepository interface to Domain layer

---
+++ src/RecipeAI.Domain/Interfaces/IPlanningSessionRepository.cs
@@ -0,0 +1,9 @@
+﻿using RecipeAI.Domain.Entities;
+
+namespace RecipeAI.Domain.Interfaces;
+
+public interface IPlanningSessionRepository
+{
+	Task<PlanningSession> AddAsync(PlanningSession session, CancellationToken cancellationToken = default);
+	Task SaveChangesAsync(CancellationToken cancellationToken = default);
+}
