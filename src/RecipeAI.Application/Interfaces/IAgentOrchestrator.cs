3079cd98118d64cc25c937eab45426d10ec370db
thekcr85 <thekcr85@gmail.com>
2026-02-28 09:59:39 +0100

    Add IAgentOrchestrator interface to Application layer

---
+++ src/RecipeAI.Application/Interfaces/IAgentOrchestrator.cs
@@ -0,0 +1,14 @@
+﻿using RecipeAI.Domain.Entities;
+using RecipeAI.Domain.Enums;
+
+namespace RecipeAI.Application.Interfaces;
+
+public interface IAgentOrchestrator
+{
+	Task<MealPlan> CreateMealPlanAsync(
+		DietType dietType,
+		int numberOfDays,
+		int targetCalories,
+		decimal budgetLimit,
+		CancellationToken cancellationToken = default);
+}
